1cfe48561641b6f94fb52f35910391caeafb0dc3
Ricardo Ruiz <riki319@gmail.com>
2018-04-09 23:39:56 +0200

    Python project structure, pip installable

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+        name="git-quotes",
+        version="0.1",
+        packages=find_packages(),
+        include_package_data=True,
+        install_requires=[
+            'click'
+        ],
+        entry_points='''
+            [console_scripts]
+            git-quotes=quotes.git_quotes:cli
+        ''',
+        data_files=[
+            ('hooks', ['quotes/hooks/prepare-commit-msg']),
+            ('hooks', ['quotes/hooks/quotes.json'])
+        ]
+)
